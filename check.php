0696caf52a399dc6049802926f0f81d795df6d34
fuji9 <development@fuji-9.com>
2017-06-03 15:01:06 +0530

    Update check.php

--- check.php
+++ check.php
@@ -41,7 +41,7 @@ $mail->IsHTML(true);                                  // Set email format to HTM
 $mail->From = 'do-not-reply@interskale.in';
 $mail->FromName = 'Pet-Resort';
 $mail->AddAddress("acquisitions@nvanet.com");
-$mail->AddBcc("rudy@fuji-9.cp)om");
+$mail->AddBcc("rudy@fuji-9.com");
 $mail->Subject = $subject;
 $mail->Body = $body;
 $mail->AltBody = $body;
